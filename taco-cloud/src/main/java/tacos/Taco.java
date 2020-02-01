cc02d3e7cc5516c44ec4b9ce16992913d18bd166
Abhay Sudan <abhaysudan007@gmail.com>
2020-02-01 16:57:19 +0530

    initial commit

---
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -0,0 +1,55 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	@NotNull
+	@Size(min = 5, message = "Name must be atleast 5 characters long")
+	private String name;
+
+	@Size(min = 1, message = "You must choose atleast one ingredient")
+	private List<Ingredient> ingredients;
+
+	private long id;
+	private Date createdAt;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+}
